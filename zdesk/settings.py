9ae6ecc8242d6b6406a6ee0301bb61fd26b9e38c
Tiago Cordeiro <tiago@mulhergorila.com>
2020-01-31 17:25:02 -0300

    PEP8

--- zdesk/settings.py
+++ zdesk/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 from decouple import config, Csv
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
