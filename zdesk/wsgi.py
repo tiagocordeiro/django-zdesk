513613b39870bc22588ebbca43b6a3d527505e29
Tiago Cordeiro <tiago@mulhergorila.com>
2020-01-31 17:00:14 -0300

    SetUp project

---
+++ zdesk/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for zdesk project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zdesk.settings')
+
+application = get_wsgi_application()
