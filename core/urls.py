9c31c15cdae5eeed05fcfeb6e59bad0f55695745
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-01 01:49:06 -0300

    front end mock up
    This close #3

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.dashboard, name='dashboard'),
+    path('profile/', views.profile_edit, name='profile_edit'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'core/index.html')
+@login_required
+def dashboard(request):
+    return render(request, 'core/dashboard.html',)
+
+
+@login_required
+def profile_edit(request):
+    return render(request, 'core/profile.html')
