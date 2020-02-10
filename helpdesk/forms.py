1a349324343e11394accc83f797c3cd36e46ef54
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-10 00:06:31 -0300

    New ticket by staff logged user.
    Adicionado views, templates para adicionar novo ticket no dashboard.
    
    This close #24

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Select, CheckboxInput, Textarea, TextInput
+from django.forms import ModelForm, Select, CheckboxInput, Textarea, TextInput, EmailInput, CheckboxSelectMultiple
 
 from .models import Ticket
 
@@ -16,6 +16,42 @@ class TicketManageForm(ModelForm):
                   'resolution_report', ]
 
         widgets = {
+            'status': Select(attrs={'class': 'form-control', 'placeholder': 'Status'}),
+            'priority': Select(attrs={'class': 'form-control', 'placeholder': 'Prioridade'}),
+            'tecnico_pre_diagnostico': Select(attrs={'class': 'form-control', 'placeholder': 'Pré diagnostico'}),
+            'tecnico_de_campo': Select(attrs={'class': 'form-control', 'placeholder': 'Tecnico de campo'}),
+            'is_customer': CheckboxInput(attrs={'class': 'form-control'}),
+            'customer_code': TextInput(attrs={'class': 'form-control'}),
+            'need_paper': CheckboxInput(attrs={'class': 'form-control'}),
+            'resolution_report': Textarea(attrs={'class': 'form-control'})
+        }
+
+
+class TicketForm(ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['queue',
+                  'problems',
+                  'submitter_name',
+                  'submitter_company',
+                  'submitter_phone',
+                  'submitter_email',
+                  'status',
+                  'priority',
+                  'tecnico_pre_diagnostico',
+                  'tecnico_de_campo',
+                  'is_customer',
+                  'customer_code',
+                  'need_paper',
+                  'resolution_report', ]
+
+        widgets = {
+            'queue': Select(attrs={'class': 'form-control', 'placeholder': 'Queue'}),
+            'problems': CheckboxSelectMultiple(attrs={'class': 'form-control'}),
+            'submitter_name': TextInput(attrs={'class': 'form-control'}),
+            'submitter_company': TextInput(attrs={'class': 'form-control'}),
+            'submitter_phone': TextInput(attrs={'class': 'form-control'}),
+            'submitter_email': EmailInput(attrs={'class': 'form-control'}),
             'status': Select(attrs={'class': 'form-control', 'placeholder': 'Status'}),
             'priority': Select(attrs={'class': 'form-control', 'placeholder': 'Prioridade'}),
             'tecnico_pre_diagnostico': Select(attrs={'class': 'form-control', 'placeholder': 'Pré diagnostico'}),
--- helpdesk/tests.py
+++ helpdesk/tests.py
@@ -1,8 +1,15 @@
+import base64  # for decoding base64 image
 import re
+from io import BytesIO
 
+from django.contrib.auth.models import AnonymousUser, User, Group
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.test import TestCase, RequestFactory, Client
+from django.urls import reverse
 
-from helpdesk.models import Queue, QueueQuestion, make_secret
+from helpdesk.models import Queue, QueueQuestion, make_secret, Ticket
+from .forms import TicketForm
+from .views import ticket_crete, ticket_detail, ticket_add
 
 
 class HelpdeskTestCase(TestCase):
@@ -11,8 +18,20 @@ class HelpdeskTestCase(TestCase):
         self.factory = RequestFactory()
         self.client = Client()
 
+        image_thumb = '''R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'''.strip()
+        self.image = InMemoryUploadedFile(
+            BytesIO(base64.b64decode(image_thumb)),  # use io.BytesIO
+            field_name='tempfile',
+            name='tempfile.png',
+            content_type='image/png',
+            size=len(image_thumb),
+            charset='utf-8',
+        )
+
         # Queue
-        self.queue_cnc = Queue.objects.create(title='Teste Router CNC')
+        self.queue_cnc = Queue.objects.create(title='Teste Router CNC',
+                                              description='Um fila de tickets para testes',
+                                              image=str(self.image))
 
         # Queue Questions and Problems
         self.queue_cnc_question_01 = QueueQuestion.objects.create(question='Máquina não liga',
@@ -23,6 +42,20 @@ class HelpdeskTestCase(TestCase):
                                                                   queue=self.queue_cnc)
         self.queue_cnc_question_04 = QueueQuestion.objects.create(question='Spindle não Gira',
                                                                   queue=self.queue_cnc)
+
+        # Ticket
+        self.ticket_non_customer = Ticket.objects.create(queue=self.queue_cnc,
+                                                         problems=[self.queue_cnc_question_03,
+                                                                   self.queue_cnc_question_04],
+                                                         submitter_name='Non Customer',
+                                                         submitter_company='New Company',
+                                                         submitter_phone='(11) 12213-0987',
+                                                         submitter_email='non@foo.bar')
+
+        # Staff user
+        self.user_staff = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user_staff)
 
     def test_retorno_queue_title(self):
         queue = Queue.objects.first()
@@ -45,3 +78,168 @@ class HelpdeskTestCase(TestCase):
 
         self.assertIsNotNone(uuid_valid_format.match(secret))
         self.assertEqual(len(secret), 36)
+
+    def test_public_view_ticket_create_step_one_status_code(self):
+        request = self.factory.get('/ticket/create/')
+        request.user = AnonymousUser()
+
+        response = ticket_crete(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_public_view_ticket_create_step_one_content(self):
+        request = self.factory.get('/ticket/create/')
+        request.user = AnonymousUser()
+
+        response = ticket_crete(request)
+        self.assertContains(response, 'Teste Router CNC')
+
+    def test_public_view_ticket_create_step_two_status_code(self):
+        request = self.factory.get('/ticket/create/new/')
+        request.user = AnonymousUser()
+
+        response = self.client.post('/ticket/create/new/', data={'maquina': self.queue_cnc})
+        self.assertEqual(response.status_code, 200)
+
+    def test_public_view_ticket_create_step_two_content(self):
+        request = self.factory.get('/ticket/create/new/')
+        request.user = AnonymousUser()
+
+        response = self.client.post('/ticket/create/new/', data={'maquina': self.queue_cnc})
+        self.assertContains(response, 'Máquina não liga')
+
+    def test_public_view_new_ticket_create_final_step(self):
+        new_ticket_data = {
+            'queue': self.queue_cnc,
+            'name': 'Benjamin',
+            'company': 'Dharma',
+            'mobile': '(11) 12345-6789',
+            'email': 'ben@dharma.org',
+            'cliente': '',
+            'problemas': [self.queue_cnc_question_01, self.queue_cnc_question_03, 'Outros'],
+            'outros problemas': 'Mais um problema',
+        }
+
+        open_tickets = Ticket.objects.all()
+        self.assertEqual(len(open_tickets), 1)
+
+        response = self.client.post(reverse('public_ticket_create'), data=new_ticket_data)
+        self.assertEqual(response.status_code, 200)
+
+        open_tickets = Ticket.objects.all()
+        self.assertEqual(len(open_tickets), 2)
+
+        new_ticket_data_customer_default_problem = {
+            'queue': self.queue_cnc,
+            'name': 'Michel',
+            'company': 'Opus',
+            'mobile': '(12) 22342-6229',
+            'email': 'michel@opus.net',
+            'cliente': 'on',
+            'problemas': [self.queue_cnc_question_02],
+        }
+
+        response = self.client.post(reverse('public_ticket_create'), data=new_ticket_data_customer_default_problem)
+        self.assertEqual(response.status_code, 200)
+
+        open_tickets = Ticket.objects.all()
+        self.assertEqual(len(open_tickets), 3)
+
+    def test_public_view_ticket_detail_status_code(self):
+        email = self.ticket_non_customer.submitter_email
+        secret = self.ticket_non_customer.secret_key
+        request = self.factory.get(f'ticket/{email}/{secret}/')
+        request.user = AnonymousUser()
+
+        response = ticket_detail(request, email=email, secret=secret)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Non Customer')
+
+    def test_view_for_ticket_create_by_staff_with_non_logged_user(self):
+        self.client.logout()
+        response = self.client.get(reverse('ticket_add'))
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response,
+                             '/accounts/login/?next=/ticket/add/',
+                             status_code=302,
+                             target_status_code=200)
+
+    def test_view_for_ticket_create_by_staff_user_logged_status_code(self):
+        request = self.factory.get(reverse('ticket_add'))
+        request.user = self.user_staff
+
+        response = ticket_add(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_ticket_form_is_valid(self):
+        form_data = {
+            'queue': self.queue_cnc,
+            'problems': [self.queue_cnc_question_01, self.queue_cnc_question_03, 'Outros'],
+            'submitter_name': 'Nazir',
+            'submitter_company': 'iwAtz',
+            'submitter_phone': '(15) 4242-1337',
+            'submitter_email': 'nazir@rizan.foo',
+            'status': 1,
+            'priority': 3,
+            'tecnico_pre_diagnostico': self.user_staff,
+            'tecnico_de_campo': self.user_staff,
+            'is_customer': '',
+            'customer_code': '',
+            'need_paper': '',
+            'resolution_report': '',
+        }
+        form = TicketForm(data=form_data)
+        self.assertEqual(form.is_valid(), True)
+
+    def test_add_ticket_by_staff(self):
+        tickets = Ticket.objects.all()
+        self.assertEqual(len(tickets), 1)
+
+        new_ticket_data_by_staff = {
+            'queue': self.queue_cnc.pk,
+            'problems': [self.queue_cnc_question_01, self.queue_cnc_question_03, 'Outros'],
+            'outros problemas': 'Teste de outro',
+            'submitter_name': 'Nazir',
+            'submitter_company': 'iwAtz',
+            'submitter_phone': '(15) 4242-1337',
+            'submitter_email': 'nazir@rizan.foo',
+            'status': 1,
+            'priority': 3,
+            'tecnico_pre_diagnostico': self.user_staff.pk,
+            'tecnico_de_campo': self.user_staff.pk,
+            'is_customer': '',
+            'customer_code': '',
+            'need_paper': '',
+            'resolution_report': '',
+        }
+
+        self.client.force_login(self.user_staff)
+
+        response = self.client.post(reverse('ticket_add'), data=new_ticket_data_by_staff)
+        self.assertEqual(response.status_code, 302)
+
+        tickets = Ticket.objects.all()
+        self.assertEqual(len(tickets), 2)
+
+        new_ticket_data_by_staff_no_extra_problem = {
+            'queue': self.queue_cnc.pk,
+            'problems': [self.queue_cnc_question_01, self.queue_cnc_question_03],
+            'submitter_name': 'Rizan',
+            'submitter_company': 'zTAwi',
+            'submitter_phone': '(51) 3342-1337',
+            'submitter_email': 'rizan@narin.foo',
+            'status': 2,
+            'priority': 4,
+            'tecnico_pre_diagnostico': self.user_staff.pk,
+            'tecnico_de_campo': self.user_staff.pk,
+            'is_customer': '',
+            'customer_code': '',
+            'need_paper': '',
+            'resolution_report': '',
+        }
+
+        response = self.client.post(reverse('ticket_add'), data=new_ticket_data_by_staff_no_extra_problem)
+        self.assertEqual(response.status_code, 302)
+
+        tickets = Ticket.objects.all()
+        self.assertEqual(len(tickets), 3)
