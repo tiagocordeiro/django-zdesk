2c325544e501e2727dd3c1e4c71c6ecf1955d9df
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-10 00:06:31 -0300

    New ticket by staff logged user.
    Adicionado views, templates para adicionar novo ticket no dashboard.
    
    This close #24

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('ticket/add/', views.ticket_add, name='ticket_add'),
     path('ticket/create/', views.ticket_crete, name='ticket_create'),
     path('ticket/create/new/', views.ticket_crete_step02, name='ticket_create_step02'),
     path('ticket/create/new/done/', views.public_ticket_create, name='public_ticket_create'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('tickets/', views.ticket_list_all, name='ticket_list_all'),
     path('tickets/todo/', views.ticket_list_todo, name='ticket_list_todo'),
     path('tickets/done/', views.ticket_list_done, name='ticket_list_done'),
+    path('tickets/ajax/load-questions/', views.load_questions, name='load_questions')
 ]
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .forms import TicketManageForm
+from .forms import TicketManageForm, TicketForm
 from .models import Queue, Ticket, QueueQuestion
 
 
@@ -64,6 +64,45 @@ def public_ticket_create(request):
 def ticket_detail(request, email, secret):
     ticket = Ticket.objects.get(secret_key=secret, submitter_email=email)
     return render(request, 'helpdesk/ticket_detail.html', {'ticket': ticket})
+
+
+def load_questions(request):
+    queue = request.GET.get('queue')
+    questions = QueueQuestion.objects.filter(queue=queue)
+    return render(request, 'helpdesk/questions_builder.html', {'questions': questions})
+
+
+@login_required
+def ticket_add(request):
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+
+        if form.is_valid():
+            new_ticket = form.save(commit=False)
+
+            if 'Outros' in form.data.getlist('problems'):
+                problems = form.data.getlist('problems')
+                outros = form.data.get('outros problemas')
+
+                problems.remove('Outros')
+                problems.append(outros)
+            else:
+                problems = form.data.getlist('problems')
+
+            new_ticket.problems = problems
+            new_ticket.assigned_to = new_ticket.tecnico_pre_diagnostico = request.user
+
+            new_ticket.save()
+            messages.success(request, "Novo ticket cadastrado")
+            return redirect('ticket_edit', pk=new_ticket.pk)
+
+    else:
+        form = TicketForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'helpdesk/ticket_add.html', context)
 
 
 @login_required
